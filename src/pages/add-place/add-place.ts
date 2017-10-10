df74ec3a44eaf08c34bc2d3fe773da560150d5aa
pejman <naderpejman@yahoo.com>
2017-10-10 10:36:47 -0600

    183. Using a Native Device Feature: Geolocation to Locate the User
    When you click on locate me,  it gives the your location . Be sure to install npm install --save ionic-native.  this is for ionic 2 and still works. Then from the documentioan add the geolocation plugin that differs from the command max is using but still works.

--- src/pages/add-place/add-place.ts
+++ src/pages/add-place/add-place.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import {ModalController} from 'ionic-angular';
+import {Geolocation} from "ionic-native"; // this is for ionic 2 as mentioned in lecture 182
+//import { Geolocation } from '@ionic-native/geolocation'; //this is for ionic 3. the lecture is based on ionic 2 so this is commented out
 
 import {SetLocationPage} from "../set-location/set-location";
 import {Location} from "../../models/location";
@@ -32,11 +34,28 @@ export class AddPlacePage {
     modal.present();  //opnes the set-location modal
     modal.onDidDismiss( //this will be executed once the modal gets dismissed
       data=>{
-        if(data){         
+        if(data){
           this.location=data.location; //once you click confirm on teh moadal, if data is present it is passed to this
           this.locationIsSet=true;
         }
       });
+  }
+
+  onLocate(){
+    Geolocation.getCurrentPosition()  //this returns a promise. hence you can you then.
+      .then(
+        location=>{
+        this.location.lat=location.coords.latitude;
+        this.location.lng=location.coords.longitude;
+        this.locationIsSet=true;         
+        }
+      )
+      .catch(
+        error=>{
+          console.log();
+        }
+      );
+
   }
 
 
