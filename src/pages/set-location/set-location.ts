48799fa8d43719f4c3fcd117284c71dfdb582271
pejman <naderpejman@yahoo.com>
2017-10-10 08:48:03 -0600

    180. Passing the Chosen Location back to the Page
    It creates two buttons Confirm and Abort to close the modal . The confirm also passes the data to the page after closing

--- src/pages/set-location/set-location.ts
+++ src/pages/set-location/set-location.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {NavParams} from 'ionic-angular';
+import {NavParams,ViewController} from 'ionic-angular';
 
 import {Location} from "../../models/location";
 
@@ -11,7 +11,7 @@ export class SetLocationPage {
   location: Location;
   marker: Location;
 
-  constructor(private navParams: NavParams){
+  constructor(private navParams: NavParams, private  viewCtrl: ViewController){
     this.location=this.navParams.get('location');  //getting location from modals
   }
 
@@ -22,6 +22,13 @@ export class SetLocationPage {
   onSetMarker(event: any){  //event is passed in from the google map after user clicks on  passing the new lat and lng
     console.log(event);
     this.marker=new Location(event.coords.lat,event.coords.lng);
+  }
+
+  onConfirm(){
+    this.viewCtrl.dismiss({location: this.marker}); //closes while passing the data from the marker
+  }
+  onAbort(){
+    this.viewCtrl.dismiss();  //just closes the moal
   }
 
 }
